f99af30f332e5b4d95bf499b023a0c6b4aa78266
Vladimir <eurasian@list.ru>
2023-10-24 20:49:40 +0300

    First commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Xml.XPath;
+
+Console.Write("Введите основание: ");
+int baseNumber = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите степень: ");
+int degree = Convert.ToInt32(Console.ReadLine());
+
+
+int GetPow (int baseNumber, int degree)
+{
+double res = 0;
+for (int i=1; i <=degree;i++)
+{
+   res=Math.Pow(baseNumber,i);
+}
+return (int)res;
+}
+
+Console.WriteLine($"{baseNumber} в степени {degree} = {GetPow (baseNumber,  degree)}");
